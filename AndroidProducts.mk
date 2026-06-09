5f4152560e145a87fa61f20d0c9e522a501864b8
manohar199 <77186340+manohar199@users.noreply.github.com>
2026-06-09 08:37:39 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,11 +15,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_bramble.mk \
-    $(LOCAL_DIR)/aosp_bramble_64.mk \
-    $(LOCAL_DIR)/aosp_bramble_hwasan.mk \
-    $(LOCAL_DIR)/lineage_bramble.mk
-
-COMMON_LUNCH_CHOICES := \
-    aosp_bramble-trunk_staging-userdebug \
-    lineage_bramble-trunk_staging-userdebug
+    $(LOCAL_DIR)/custom_bramble.mk 
