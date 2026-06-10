c632537a948b139103e2e11bd6c8ebd18d6872bc
manohar199 <77186340+manohar199@users.noreply.github.com>
2026-06-10 19:00:55 +0530

    Update device-bramble.mk

--- device-bramble.mk
+++ device-bramble.mk
@@ -181,3 +181,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     NfcOverlayBramble
+
+# OTA Updater
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    lineage.updater.uri=https://raw.githubusercontent.com/manohar199/ota_bramble/main/bramble.json
